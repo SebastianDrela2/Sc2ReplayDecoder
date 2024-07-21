12138d26b5464dc7008870bf9c6e54615be2e156
Sebastian Drela <sebastian.drela@wp.pl>
2024-07-21 05:23:36 +0200

    fix types

--- MPQArchive/MPQ/ReceivedData/MPQArchive.cs
+++ MPQArchive/MPQ/ReceivedData/MPQArchive.cs
@@ -4,9 +4,9 @@
     {
         public MPQHeader1 MPQHeader { get; set; }
         public MPQUserData MPQUserData { get; set; }
-        public Dictionary<string, string[]> ListingFiles { get; set; }
+        public Dictionary<string, byte[]> ListingFiles { get; set; }
       
-        public MPQArchive(MPQHeader1 mpqHeader, MPQUserData mpqUserData, Dictionary<string, string[]> listingFiles)
+        public MPQArchive(MPQHeader1 mpqHeader, MPQUserData mpqUserData, Dictionary<string, byte[]> listingFiles)
         {
             MPQHeader = mpqHeader ?? throw new ArgumentNullException(nameof(mpqHeader));
             MPQUserData = mpqUserData ?? throw new ArgumentNullException(nameof(mpqUserData));
